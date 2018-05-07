796c2fd3da3f70a0b063393220267dfc75ed35e6
p <koldfuzor@gmail.com>
2018-05-07 14:41:27 -0700

    Update renamed view.h

--- cb.cpp
+++ cb.cpp
@@ -20,7 +20,7 @@
 #include "core/web/dom.h"
 #include "core/web/css.h"
 #include "core/app/flow.h"
-#include "core/app/gui.h"
+#include "core/app/gl/view.h"
 #include "core/app/network.h"
 
 namespace LFL {
