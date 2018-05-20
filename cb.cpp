ac9bc2cfd94373235d8978d0816b48bd6c7d2b1b
Justin Foutts <jfoutts@tripwire.com>
2018-05-19 20:09:00 -0700

    Derive MyApp from Application

--- cb.cpp
+++ cb.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: cb.cpp 1336 2014-12-08 09:29:59Z justin $
+ * $Id$
  * Copyright (C) 2009 Lucid Fusion Labs
 
  * This program is free software: you can redistribute it and/or modify
@@ -22,14 +22,19 @@
 #include "core/app/flow.h"
 #include "core/app/gl/view.h"
 #include "core/app/network.h"
+#include "core/app/shell.h"
 
 namespace LFL {
 DEFINE_FLAG(sniff_device, int, 0, "Network interface index");
 
-struct MyAppState {
+struct MyApp : public Application {
+  using Application::Application;
   unique_ptr<Sniffer> sniffer;
   unique_ptr<GeoResolution> geo;
-} *my_app = new MyAppState();
+  void OnWindowInit(Window *W);
+  void OnWindowStart(Window *W);
+  void Sniff(const char *packet, int avail, int size);
+} *app;
 
 struct MyView : public View {
   Scene scene;
@@ -46,15 +51,15 @@ struct MyView : public View {
   }
 };
 
-void Sniff(const char *packet, int avail, int size) {
+void MyApp::Sniff(const char *packet, int avail, int size) {
   if (avail < Ethernet::Header::Size + IPV4::Header::MinSize) return;
   IPV4::Header *ip = (IPV4::Header*)(packet + Ethernet::Header::Size);
   int iphdrlen = ip->hdrlen() * 4;
   if (iphdrlen < IPV4::Header::MinSize || avail < Ethernet::Header::Size + iphdrlen) return;
   string src_ip = IPV4::Text(ip->src), dst_ip = IPV4::Text(ip->dst), src_city, dst_city;
   float src_lat, src_lng, dst_lat, dst_lng;
-  my_app->geo->Resolve(src_ip, 0, 0, &src_city, &src_lat, &src_lng);
-  my_app->geo->Resolve(dst_ip, 0, 0, &dst_city, &dst_lat, &dst_lng);
+  geo->Resolve(src_ip, 0, 0, &src_city, &src_lat, &src_lng);
+  geo->Resolve(dst_ip, 0, 0, &dst_city, &dst_lat, &dst_lng);
 
   if (ip->prot == 6 && avail >= Ethernet::Header::Size + iphdrlen + TCP::Header::MinSize) {
     TCP::Header *tcp = (TCP::Header*)((char*)ip + iphdrlen);
@@ -67,13 +72,13 @@ void Sniff(const char *packet, int avail, int size) {
   else INFO("ip ver=", ip->version(), " prot=", ip->prot, " ", src_ip, " -> ", dst_ip);
 }
 
-void MyWindowInit(Window *W) {
-  W->width = 420;
-  W->height = 380;
+void MyApp::OnWindowInit(Window *W) {
+  W->gl_w = 420;
+  W->gl_h = 380;
   W->caption = "CrystalBawl";
 }
 
-void MyWindowStart(Window *W) {
+void MyApp::OnWindowStart(Window *W) {
   CHECK_EQ(0, W->NewView());
   MyView *view = W->ReplaceView(0, make_unique<MyView>(W));
   W->frame_cb = bind(&MyView::Frame, view, _1, _2, _3);
@@ -96,16 +101,16 @@ void MyWindowStart(Window *W) {
 }; // namespace LFL
 using namespace LFL;
 
-extern "C" void MyAppCreate(int argc, const char* const* argv) {
+extern "C" LFApp *MyAppCreate(int argc, const char* const* argv) {
   FLAGS_enable_video = FLAGS_enable_input = FLAGS_enable_network = 1;
   FLAGS_target_fps = 50;
   FLAGS_threadpool_size = 1;
-  app = new Application(argc, argv);
-  app->focused = Window::Create();
-  app->window_start_cb = MyWindowStart;
-  app->window_init_cb = MyWindowInit;
+  app = make_unique<MyApp>(argc, argv).release();
+  app->focused = CreateWindow(app).release();
+  app->window_start_cb = bind(&MyApp::OnWindowStart, app, _1);
+  app->window_init_cb = bind(&MyApp::OnWindowInit, app, _1);
   app->window_init_cb(app->focused);
-  app->exit_cb = []{ delete my_app; };
+  return app;
 }
 
 extern "C" int MyAppMain() {
@@ -113,28 +118,28 @@ extern "C" int MyAppMain() {
   if (app->Init()) return -1;
 
   // app->asset.Add(name, texture,     scale, translate, rotate, geometry,                 nullptr, 0, 0);
-  app->asset.Add("axis",  "",          0,     0,         0,      nullptr,                  nullptr, 0, 0, Asset::DrawCB(bind(&glAxis, _1, _2, _3)));
-  app->asset.Add("grid",  "",          0,     0,         0,      Grid::Grid3D().release(), nullptr, 0, 0);
-  app->asset.Add("room",  "",          0,     0,         0,      nullptr,                  nullptr, 0, 0, Asset::DrawCB(bind(&glRoom, _1, _2, _3)));
-  app->asset.Add("arrow", "",         .005,   1,        -90,     "arrow.obj",              nullptr, 0);
+  app->asset.Add(app, "axis",  "",          0,     0,         0,      nullptr,                  nullptr, 0, 0, Asset::DrawCB(bind(&glAxis, _1, _2, _3)));
+  app->asset.Add(app, "grid",  "",          0,     0,         0,      Grid::Grid3D().release(), nullptr, 0, 0);
+  app->asset.Add(app, "room",  "",          0,     0,         0,      nullptr,                  nullptr, 0, 0, Asset::DrawCB(bind(&glRoom, _1, _2, _3)));
+  app->asset.Add(app, "arrow", "",         .005,   1,        -90,     "arrow.obj",              nullptr, 0);
   app->asset.Load();
 
   // app->soundassets.push_back(SoundAsset(name, filename,   ringbuf, channels, sample_rate, seconds));
-  app->soundasset.Add(SoundAsset("draw",  "Draw.wav", 0,       0,        0,           0      ));
+  app->soundasset.Add(SoundAsset(app, "draw",  "Draw.wav", 0,       0,        0,           0      ));
   app->soundasset.Load();
 
   app->StartNewWindow(app->focused);
   MyView *view = app->focused->GetOwnView<MyView>(0);
-  view->scene.Add(new Entity("axis",  app->asset("axis")));
-  view->scene.Add(new Entity("grid",  app->asset("grid")));
-  view->scene.Add(new Entity("room",  app->asset("room")));
-  view->scene.Add(new Entity("arrow", app->asset("arrow"), v3(1, .24, 1)));
+  view->scene.Add(make_unique<Entity>("axis",  app->asset("axis")));
+  view->scene.Add(make_unique<Entity>("grid",  app->asset("grid")));
+  view->scene.Add(make_unique<Entity>("room",  app->asset("room")));
+  view->scene.Add(make_unique<Entity>("arrow", app->asset("arrow"), v3(1, .24, 1)));
 
   vector<string> devices;
   Sniffer::PrintDevices(&devices);
   if (FLAGS_sniff_device < 0 || FLAGS_sniff_device >= devices.size()) FATAL(FLAGS_sniff_device, " oob ", devices.size(), ", are you running as root?");
-  if (!(my_app->sniffer = Sniffer::Open(devices[FLAGS_sniff_device], "", 1024, Sniff))) FATAL("sniffer Open failed");
-  if (!(my_app->geo = GeoResolution::Open(StrCat(app->assetdir, "GeoLiteCity.dat").c_str()))) FATAL("geo Open failed");
+  if (!(app->sniffer = Sniffer::Open(devices[FLAGS_sniff_device], "", 1024, bind(&MyApp::Sniff, app, _1, _2, _3)))) FATAL("sniffer Open failed");
+  if (!(app->geo = GeoResolution::Open(StrCat(app->assetdir, "GeoLiteCity.dat").c_str()))) FATAL("geo Open failed");
 
   return app->Main();
 }
